2c47a2da12c257771e6849a031a893440666689e
longbinfeng <binfeng.long@hand-china.com>
2021-09-14 21:28:10 +0800

    初始化仓库：组件库搭建

---
+++ .umirc.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'rzh-design',
+  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  // 配置文档的Logo
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  // 指定输出路径
+  outputPath: 'docs-dist',
+  // 设定文档的展现模式
+  mode: 'site',
+  // more config: https://d.umijs.org/config
+});
---
+++ README.md
@@ -0,0 +1,27 @@
+# rzh-design
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ npm i
+```
+
+Start the dev server,
+
+```bash
+$ npm start
+```
+
+Build documentation,
+
+```bash
+$ npm run docs:build
+```
+
+Build rzh-design via `father-build`,
+
+```bash
+$ npm run build
+```
